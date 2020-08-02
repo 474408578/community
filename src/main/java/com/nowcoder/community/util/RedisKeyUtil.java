45bb8e32b7856314d46a26af9220550979a7adaa
xiangsong <1638392300@qq.com>
2020-08-02 10:01:27 +0800

    增加关注和取消关注逻辑

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -15,8 +15,11 @@ public class RedisKeyUtil {
 
     private static final String PREFIX_USER = "like:user";
 
+    private static final String PREFIX_FOLLOWEE = "followee";
+    private static final String PREFIX_FOLLOWER = "follower";
+
     // 某个实体的赞, 得出实体的赞的key，set集合表示点赞的人。
-    // key: like:entity:entityType:entityId -> value: set(userId)  set集合作为value
+    // like:entity:entityType:entityId -> set(userId)  set集合作为value
     public static String getEntityLikeKey(int entityType, int entityId) {
         return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
     }
@@ -26,6 +29,16 @@ public class RedisKeyUtil {
         return PREFIX_USER + SPLIT + userId;
     }
 
+    // 某个用户关注的实体
+    // followee:userId:entityType -> zset(entityId, now)
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
 
+    // 某个实体拥有的粉丝
+    // follower:entityType:entityId -> zset(userId, now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
+    }
 
 }
