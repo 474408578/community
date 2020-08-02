fa6850184c3da3b815a32ab5f0bf540b6be53952
xiangsong <1638392300@qq.com>
2020-08-02 20:35:51 +0800

    整合了kafka

---
+++ src/test/java/com/nowcoder/community/KafkaTests.java
@@ -0,0 +1,61 @@
+package com.nowcoder.community;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author xschen
+ *
+ *  kafka发送和接收消息测试
+ *      发消息是主动的过程，接收消息不需要主动去调用方法
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class KafkaTests {
+
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    @Test
+    public void kafkaTests() {
+        kafkaProducer.sendMessage("test", "你好");
+        kafkaProducer.sendMessage("test", "在吗");
+
+        try {
+            Thread.sleep(1000 * 10);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+@Component
+class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate kafkaTemplate;
+
+    public void sendMessage(String topic, String content) {
+        kafkaTemplate.send(topic, content);
+    }
+}
+
+@Component
+class KafkaConsumer {
+
+    @KafkaListener(topics = {"test"})
+    public void handleMessage(ConsumerRecord record) {
+        System.out.println("消费到消息：" + record.value());
+    }
+
+}
